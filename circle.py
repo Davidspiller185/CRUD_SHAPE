eddcf8059a138768131165d24bdae9f0eb09b830
david <dspiller321@gmail.com>
2026-05-29 12:12:12 +0300

    add function to calculate area permiter,and toenter into dict

--- circle.py
+++ circle.py
@@ -1,5 +1,16 @@
 from shape import Shape
-class circle(Shape):
+class Circle(Shape):
     def __init__(self, shape_id, shape_type,radius):
         super().__init__(shape_id, shape_type)
         self.radius=radius
+
+    def get_area(self):
+        area=3.14 * self.radius**2
+        return area
+    
+    def get_perimeter(self):
+        perimeter=2 * 3.14 * self.radius
+        return perimeter
+    
+    def to_dict(self):
+        return {"id":self.id,"type":self.shape_type,"radius":self.radius,"Area":self.get_area(),"perimeter":self.get_perimeter()}
