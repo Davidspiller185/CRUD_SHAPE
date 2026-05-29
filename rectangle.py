80b88adefdc73d33deb955af217180195b3f7b0e
david <dspiller321@gmail.com>
2026-05-29 12:16:20 +0300

    add function calculae area,perimeter, and enter into dict

--- rectangle.py
+++ rectangle.py
@@ -4,3 +4,13 @@ class Rectangle(Shape):
         super().__init__(shape_id, shape_type)
         self.width=width
         self.height=height
+
+    def get_area(self):
+        area=self.width *self.height
+        return area
+    
+    def get_perimeter(self):
+        perimeter= 2 * (self.width + self.height)
+        return perimeter
+    def to_dict(self):
+        return {"id":self.id,"type":self.shape_type,"width":self.width,"heigt":self.height,"area":self.get_area(),"perimeter":self.get_perimeter()}
