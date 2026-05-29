25132a4e8d435730f1da4e57d0fe5a7ac5bf4ae9
david <dspiller321@gmail.com>
2026-05-29 12:24:22 +0300

    add function calculat area,perimeter, and enter to a dict

--- square.py
+++ square.py
@@ -3,3 +3,12 @@ class Square(Shape):
     def __init__(self, shape_id, shape_type,side):
         super().__init__(shape_id, shape_type)
         self.side=side
+    def get_area(self):
+        area=self.side * self.side
+        return area
+    def get_perimeter(self):
+        perimeter=4 * self.side
+        return perimeter
+    
+    def to_dict(self):
+        return {"id":self.id,"type":self.shape_type,"side":self.side,"area":self.get_area(),"perimeter":self.get_perimeter()}
