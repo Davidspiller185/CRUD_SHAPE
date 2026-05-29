42305be926070805d0db3bef3d5257eb210819e2
david <dspiller321@gmail.com>
2026-05-29 12:26:19 +0300

    add function calculate area,perimeter,and enter into dict

--- triangle.py
+++ triangle.py
@@ -7,3 +7,11 @@ class Triangle(Shape):
         self.side_a=side_a
         self.side_b=side_b
         self.side_c=side_c
+    def get_area(self):
+        area=(self.base * self.height)
+        return area
+    def get_perimeter(self):
+        perimeter=self.side_a + self.side_b + self.side_c
+        return perimeter
+    def to_dict(self):
+        return {"id":self.id,"type":self.shape_type,"base":self.base,"heigt":self.height,"side_a":self.side_a,"side_b":self.side_b,"side_c":self.side_c,"area":self.get_area(),"perimeter":self.get_perimeter()}
