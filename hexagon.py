235aef93565602277850b9faf038647d450ed587
david <dspiller321@gmail.com>
2026-05-29 12:13:39 +0300

    add function calculat area,perimeter, and enter into dict

--- hexagon.py
+++ hexagon.py
@@ -1,5 +1,17 @@
 from shape import Shape
+from math import sqrt
 class Hexagon(Shape):
     def __init__(self, shape_id, shape_type,side):
         super().__init__(shape_id, shape_type)
         self.side=side
+    
+    def get_area(self):
+        area=(3 * sqrt(3) *self.side**2)/2
+        return area
+    
+    def get_perimeter(self):
+        perimeter=6 * self.side
+        return perimeter
+    
+    def to_dict(self):
+        return {"id":self.id,"type":self.shape_type,"side":self.side,"Area":self.get_area(),"Perimeter":self.get_perimeter()}
